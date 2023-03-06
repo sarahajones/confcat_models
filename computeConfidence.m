dba7b9d1bd455e14b5e0d91fd9fc2f21e90f74da
Sarah Ashcroft-Jones <sarah.ashcroft-jones@psy.ox.ac.uk>
2023-03-06 14:17:02 +0000

    update
    2 bayes models, BIC calculations

--- computeConfidence.m
+++ computeConfidence.m
@@ -8,8 +8,8 @@ metaNoise = (randn(1, nTrials)*(Data.metacognitiveNoise))'; %computes norm dist 
 
 vector1 = Model ==1 & Decision ==1; 
 vector2 = Model ==1 & Decision ==0;
-vector3 = Model ==2 & Decision ==1 | Model ==2 & Decision ==0; 
-vector4 = Model ==3 & Decision ==1 | Model ==3 & Decision ==0;  
+vector3 = Model ==2 & Decision ==1; 
+vector4 = Model ==2 & Decision ==0;  
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %MODEL 1: compute the Bayesian posteriors
@@ -29,21 +29,57 @@ posteriorRatio(vector2,1) = log(numerator(vector2,1)./denominator(vector2,1));
 posteriorRatio(vector2,1) = posteriorRatio(vector2,1) + metaNoise(vector2,1);
 conf(vector2,1) = (1 ./(1 + exp(posteriorRatio(vector2,1))));
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%MODEL 2: compute the distance to decision bound model (at balance point of 350)
-distance(vector3,1) = abs(Data.Percept(vector3,1) - 350);
-conf(vector3,1) = tanh(distance(vector3,1)) + metaNoise(vector3,1);
-
-% posteriorRatio(vector3,1) = -(log(normcdf(350, -Data.Percept(vector3, 1), Data.Sigma_X)./(1 - (normcdf(350, -Data.Percept(vector3, 1), Data.Sigma_X)))));
-% posteriorRatio(vector3,1) = posteriorRatio(vector3,1) + metaNoise(vector3,1);
-% conf(vector3,1) = (1 ./(1 + (posteriorRatio(vector3,1))));
-% 
-% posteriorRatio(vector4,1) = log((normcdf(350, -Data.Percept(vector4, 1), Data.Sigma_X))./(1 - (normcdf(350, -Data.Percept(vector4, 1), Data.Sigma_X))));
-% posteriorRatio(vector4,1) = posteriorRatio(vector4,1) + metaNoise(vector4,1);
-% conf(vector4,1)= (1 ./(1 + (posteriorRatio(vector4,1))));
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%MODEL 3: compute distance to midline between means model
-midline = abs(mu_cat1-mu_cat2);
-distance(vector4,1) = abs(Data.Percept(vector4,1) - midline);
-conf(vector4,1) = tanh(distance(vector4,1)) + metaNoise(vector4,1);
+%MODEL 2: compute the Bayesian posteriors (SYMMETRICAL)
+%we are building a BAyesian approximation of Confidence when the 2 means
+%and 2 sigmas are SAME (AVERAGE OF EACH) (for each category)
+avg_sigma = (sigma_s1+ sigma_s2)/2;
+
+%first if decision == 1
+numerator(vector3,1) = ((1)/(sqrt(2*pi*(avg_sigma^2)))).*exp(-((Data.Percept(vector3) + Data.Location(vector3)).^2)./(2*(avg_sigma)^2));
+denominator(vector3,1) = ((1)/(sqrt(2*pi*(avg_sigma^2))))*exp(-((Data.Percept(vector3) + Data.Location(vector3)).^2)./(2*(avg_sigma)^2));
+posteriorRatio(vector3,1) = log(numerator(vector3,1)./denominator(vector3,1));
+posteriorRatio(vector3,1) = posteriorRatio(vector3,1) + metaNoise(vector3,1);
+conf(vector3,1) = (1 ./(1 + exp(posteriorRatio(vector3,1))));
+  
+%now if decision ==2
+numerator(vector4,1) = ((1)/(sqrt(2*pi*(avg_sigma^2)))).*exp(-((Data.Percept(vector4) + Data.Location(vector4)).^2)./(2*(avg_sigma)^2));
+denominator(vector4,1) = ((1)/(sqrt(2*pi*(avg_sigma^2)))).*exp(-((Data.Percept(vector4) + Data.Location(vector4)).^2)./(2*(avg_sigma)^2));
+posteriorRatio(vector4,1) = log(numerator(vector4,1)./denominator(vector4,1));
+posteriorRatio(vector4,1) = posteriorRatio(vector4,1) + metaNoise(vector4,1);
+conf(vector4,1) = (1 ./(1 + exp(posteriorRatio(vector4,1))));
+
+
+
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %MODEL 2: compute the distance to decision bound model (at balance point of
+% %350) aand translate to confidence
+% distance(vector3,1) = abs(Data.Percept(vector3,1) - 350);
+% %need to find a way to make this distance into confidence between 0 and 1
+% %distance is to the midline at 350 , the space is 0-800 so 800 untis widde,
+% %the largest distance we can have is 450. 
+% conf(vector3,1) = distance(vector3,1)./350 ;
+% conf(vector3,1) = (conf(vector3,1).*100) + metaNoise(vector3,1);
+% if conf(vector3,1) < 0
+%     conf(vector3,1) = 0;
+% end
+% conf(vector3,1) = conf(vector3,1)./100;
+% 
+% 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %MODEL 3: compute distance to midline between means model and translate to
+% %confidence
+% 
+% midline = abs(mu_cat1-mu_cat2);
+% midline = mu_cat1 + (midline/2);
+% distance(vector4,1) = abs(Data.Percept(vector4,1) - midline);
+% conf(vector4,1) = distance(vector4,1)./midline ;
+% conf(vector4,1) = (conf(vector4,1).*100) + metaNoise(vector4,1);
+% if conf(vector4,1) < 0
+%     conf(vector4,1) = 0;
+% end
+% 
+% 
+% conf(vector4,1) = conf(vector4,1)./100;
+
